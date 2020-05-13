2e3a9960c37acee152efefacfc7c1b1a1f194f5d
LRydin <leonardo.rydin@gmail.com>
2020-05-13 16:27:38 +0200

    Called the wrong functions for the test. corrected.

--- test/formulae_test.py
+++ test/formulae_test.py
@@ -1,5 +1,5 @@
 import numpy as np
-from JumpDiff.formulae import Qratio, jdprocess
+from JumpDiff.formulae import M_formula, F_formula, F_formula_solver
 
 def test_fomulae():
     for power in [1,2,3,4,5,6,7,8,9]:
@@ -7,5 +7,5 @@ def test_fomulae():
         M_formula(power = power)
 
         F_formula(power = power)
-        
+
         F_formula_solver(power = power)
