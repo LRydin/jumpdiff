93774ffff2fac32d2935a3e6dbe75ee111adfcf0
LRydin <leonardo.rydin@gmail.com>
2022-11-14 17:31:46 +0100

    Checking library version all python versions. Testing back 3.4 and 3.5

---
+++ test/versioning_libraries.py
@@ -0,0 +1,9 @@
+import sys
+import numpy
+import scipy
+import sympy
+
+print('python:', sys.version)
+print('numpy:', numpy.__version__)
+print('scipy:', scipy.__version__)
+print('sympy:', sympy.__version__)
