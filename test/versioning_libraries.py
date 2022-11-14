2c2d9b4dc1d8cc0880fb0126c1f46215c948ccef
LRydin <leonardo.rydin@gmail.com>
2022-11-14 17:39:25 +0100

    removing 3.4. Edited version testing.

--- test/versioning_libraries.py
+++ test/versioning_libraries.py
@@ -3,7 +3,8 @@ import numpy
 import scipy
 import sympy
 
-print('python:', sys.version)
-print('numpy:', numpy.__version__)
-print('scipy:', scipy.__version__)
-print('sympy:', sympy.__version__)
+def versioning_libraries():
+    print('python:', sys.version)
+    print('numpy:', numpy.__version__)
+    print('scipy:', scipy.__version__)
+    print('sympy:', sympy.__version__)
